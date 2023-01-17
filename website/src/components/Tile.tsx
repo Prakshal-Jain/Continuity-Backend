43f396acb29d7085d62ad2fd08cc6a0a947d2818
Prakshal-Jain <prakshal@Prakshals-MacBook-Pro.local>
2023-01-17 14:05:24 -0500

    website design fix

--- website/src/components/Tile.tsx
+++ website/src/components/Tile.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import styles from '../styles/tile.module.css';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 import React from 'react';
--- website/src/data/features.tsx
+++ website/src/data/features.tsx
@@ -1,6 +1,4 @@
-import { Props } from '../components/Tile';
-
-const video_style = { width: '60%', borderRadius: '25px', border: '0.5rem solid black' };
+import styles from '../styles/tile.module.css';
 
 export const feaures = [
     {
@@ -17,7 +15,7 @@ export const feaures = [
         title: "Built with your privacy in mind",
         description: "Continuity offers comprehensive and intelligent Privacy Reports and Tracking Protection features to protect you from the websites and services that may collect your personal information. Unlike other browsers that rely on add-on extensions to provide similar functionality, Continuity's tracking protection is deeply integrated into the browser and ensures that data is securely shared between devices without the risk of third-party access. This sets Continuity apart from other browsers and makes it a game-changing choice for users who value privacy and security.",
         video: (
-            <video autoPlay={true} muted={true} loop={true} style={video_style}>
+            <video autoPlay={true} muted={true} loop={true} className={styles.video_border}>
                 <source src="videos/privacy.mp4" type="video/mp4" />
             </video>
         )
@@ -26,7 +24,7 @@ export const feaures = [
         title: "Ultra Search",
         description: "With Ultra Search, you can find exactly what you're looking for in no time. The AI-generated suggestions are custom tailored to your specific needs, making it easier than ever to find the information you need. And for the first time ever, Ultra Search is seamlessly integrated into your web browser, making it a powerful tool for all of your online searches.",
         video: (
-            <video autoPlay={true} muted={true} loop={true} style={{ width: '60%', borderRadius: '20px', border: '0.5rem solid black' }}>
+            <video autoPlay={true} muted={true} loop={true} className={styles.video_border}>
                 <source src="videos/ultrasearch.mp4" type="video/mp4" />
             </video>
         )
