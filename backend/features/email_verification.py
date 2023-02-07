70c5f2e62c5848d897f634309b076cfcb22b1e6c
Prakshal-Jain <prakshal@Prakshals-MacBook-Pro.local>
2023-02-07 11:38:24 -0500

    added appstore playstore links and fixed email

--- backend/features/email_verification.py
+++ backend/features/email_verification.py
@@ -4,9 +4,24 @@ def email_verification(user_id, id):
     sender_email = "continuitybrowser@gmail.com"
     password = "hbjciqrivetkuuno"
     message = f"""\
-    Subject: Verify your email
+From: Continuity Browser Team <continuitybrowser@gmail.com>
+To: You {user_id}
+Subject: Continuity Email Verification
 
-    Visit: https://continuitybrowser.com/verify-email/{id} to verify."""
+Dear User,
+
+We are writing to confirm that you have recently signed up for ContinuityBrowser.
+
+To ensure the security of your account and to complete the registration process, we kindly ask you to verify your email address by clicking on the following link:
+
+https://continuitybrowser.com/verify-email/{id}
+
+
+If you did not sign up for ContinuityBrowser, please ignore this email.
+
+Best regards,
+The Continuity Browser Team
+"""
 
     try:
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
